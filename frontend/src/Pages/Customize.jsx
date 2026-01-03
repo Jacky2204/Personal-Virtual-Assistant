b9311b34b152ec25cb394bb3efc9824a7d617a66
Jacky2204 <jalindarbhapkar2@gmail.com>
2026-01-03 19:37:34 +0530

    resolve error

--- frontend/src/Pages/Customize.jsx
+++ frontend/src/Pages/Customize.jsx
@@ -9,7 +9,7 @@ import image5 from "./../assets/image5.jpg";
 import image6 from "./../assets/image6.jpg";
 import image7 from "./../assets/image7.jpeg";
 import { RiImageAddLine } from "react-icons/ri";
-import { userDataContext } from "../Context/userContext";
+import { userDataContext } from "../Context/UserContext.jsx";
 import { useNavigate } from "react-router-dom";
 import { MdKeyboardBackspace } from "react-icons/md";
 
--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import bg from "../assets/bg.mp4";
 import { useNavigate } from "react-router-dom";
-import { userDataContext } from "../Context/userContext";
+import { userDataContext } from "../Context/UserContext.jsx";
 import axios from "axios";
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";

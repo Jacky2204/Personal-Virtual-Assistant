d48631901edd4a0809637c1a21cbf154d2657605
Jacky2204 <jalindarbhapkar2@gmail.com>
2026-01-03 19:39:00 +0530

    resolve errorss

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import { userDataContext } from "../Context/userContext";
+import { userDataContext } from "../Context/UserContext.jsx";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { CgMenuRight } from "react-icons/cg";

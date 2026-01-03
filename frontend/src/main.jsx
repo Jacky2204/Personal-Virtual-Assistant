1e984caad7a54acc929f8c82b57145b8806d7250
Jacky2204 <jalindarbhapkar2@gmail.com>
2026-01-03 19:21:42 +0530

    add changess

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import UserContext from "./Context/userContext.jsx";
+import UserContext from "./Context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>

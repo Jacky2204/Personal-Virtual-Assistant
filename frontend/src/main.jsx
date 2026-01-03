7b773f954e48175db63168d547c3d886386ebee3
Jacky2204 <jalindarbhapkar2@gmail.com>
2026-01-03 18:02:39 +0530

    Fix context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { BrowserRouter } from "react-router-dom"
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import UserContext from './Context/userContext.jsx'
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import UserContext from "./Context/userContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <UserContext>
       <App />
     </UserContext>
   </BrowserRouter>
-)
+);
